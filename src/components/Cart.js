a987bad52e1c20487536196a8f539277d7eae2f5
StefiLeon <stefaniapleon@gmail.com>
2021-09-14 12:19:25 -0300

    firebase

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,6 @@
 import {useContext, useState} from "react";
 import CartContext from "../Context/CartContext";
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import ItemCount from "./ItemCount";
 import { Link } from "react-router-dom";
 
@@ -9,6 +9,11 @@ const CarritoF = () => {
     const { carrito, removeAllItems } = useContext(CartContext);
 
     const [ items, setItems ] = useState(0);
+
+    const handleCompra = (e) => {
+        e.preventDefault();
+        console.log("fin de la compra");
+    }
 
     return (
         <div>
@@ -22,7 +27,29 @@ const CarritoF = () => {
                 <p>Total del producto: {item.precio*item.count}</p>
                 </div>
             )))}
+            {carrito.length > 0 && <>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Label>Nombre y apellido</Form.Label>
+                    <Form.Control type="textarea" placeholder="Ingresá tu nombre" />
+                    <Form.Text className="text-muted">
+                    Nunca compartiremos tu información.
+                    </Form.Text>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email de contacto</Form.Label>
+                    <Form.Control type="mail" placeholder="Ingresá tu mail" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPhone">
+                    <Form.Label>Teléfono</Form.Label>
+                    <Form.Control type="phone" placeholder="Ingresá tu teléfono" />
+                </Form.Group>
+                <Button variant="primary" type="submit" onClick={handleCompra}>
+                    Terminar la compra
+                </Button>
+            </Form>
             <Button onClick={removeAllItems}>Limpiar todo el carrito</Button>
+            </>}
             {carrito.length === 0 && <>
             <h3>Carrito vacio</h3>
             <Button><Link to="/" style={{color:"white"}}>Ver productos</Link></Button>
